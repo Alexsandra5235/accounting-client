cecb1d8cefcb0ea1058b8909e7078d66c8e9449f
Alexandra Matveeva <sm1991611@gmail.com>
2025-05-20 12:36:23 +0700

    feat(телеграм): в сообщение телеграма добавлен исполнитель действия, добавлен вывод сообщения об отсутсвии данных при поиске несущуствующих записей в таблице

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Carbon\Carbon;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
 class TelegramService
@@ -18,21 +19,24 @@ class TelegramService
     }
     public function generateMessageStore($response): string
     {
+        $user = Auth::user();
         $url = "http://127.0.0.1:8000/log/{$response->id}";
         $datetime_receipt = Carbon::parse($response->log_receipt->date_receipt)->locale('ru')->translatedFormat('D, d M Y') . ' ' . $response->log_receipt->time_receipt;
         $birth_day = Carbon::parse($response->patient->birth_day)->locale('ru')->translatedFormat('D, d M Y');
-        return "👤 Новый пациент добавлен:\n\n<b>Дата и время посупления:</b> {$datetime_receipt}\n<b>ФИО:</b> {$response->patient->name}\n<b>Дата рождения:</b> {$birth_day}\n<b>Номер мед.карты:</b> {$response->patient->medical_card}\n<a href='{$url}'>Кликни, чтобы перейти</a>";
+        return "👤 Новый пациент добавлен:\n\n<b>Дата и время посупления:</b> {$datetime_receipt}\n<b>ФИО:</b> {$response->patient->name}\n<b>Дата рождения:</b> {$birth_day}\n<b>Номер мед.карты:</b> {$response->patient->medical_card}\n<a href='{$url}'>Кликни, чтобы перейти</a>\n\n👨‍⚕️👩‍⚕️ Информация о сотруднике.\n\n<b>Имя сотрудника: </b>{$user->name}\n<b>Почта сотрудника: </b>{$user->email}";
     }
     public function generateMessageDestroy($log): string
     {
-        return "🙅🏻‍♀️ Запись была удалена.\n\n<b>Имя пациента: </b>{$log->patient->name}\n<b>Номер мед.карты пациента: </b>{$log->patient->medical_card}";
+        $user = Auth::user();
+        return "🙅🏻‍♀️ Запись была удалена.\n\n<b>Имя пациента: </b>{$log->patient->name}\n<b>Номер мед.карты пациента: </b>{$log->patient->medical_card}\n\n👨‍⚕️👩‍⚕️ Информация о сотруднике.\n\n<b>Имя сотрудника: </b>{$user->name}\n<b>Почта сотрудника: </b>{$user->email}";
     }
     public function generateMessageUpdate($log): string
     {
+        $user = Auth::user();
         $url = "http://127.0.0.1:8000/log/{$log->id}";
         $datetime_receipt = Carbon::parse($log->log_receipt->date_receipt)->locale('ru')->translatedFormat('D, d M Y') . ' ' . $log->log_receipt->time_receipt;
         $birth_day = Carbon::parse($log->patient->birth_day)->locale('ru')->translatedFormat('D, d M Y');
-        return "🙊 Запись была обновлена:\n\n<b>Дата и время посупления:</b> {$datetime_receipt}\n<b>ФИО:</b> {$log->patient->name}\n<b>Дата рождения:</b> {$birth_day}\n<b>Номер мед.карты:</b> {$log->patient->medical_card}\n<a href='{$url}'>Кликни, чтобы перейти</a>";
+        return "🙊 Запись была обновлена:\n\n<b>Дата и время посупления:</b> {$datetime_receipt}\n<b>ФИО:</b> {$log->patient->name}\n<b>Дата рождения:</b> {$birth_day}\n<b>Номер мед.карты:</b> {$log->patient->medical_card}\n<a href='{$url}'>Кликни, чтобы перейти</a>\n\n👨‍⚕️👩‍⚕️ Информация о сотруднике.\n\n<b>Имя сотрудника: </b>{$user->name}\n<b>Почта сотрудника: </b>{$user->email}";
     }
     public function sendMessage(string $message): void
     {
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -110,54 +110,62 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <header class="mb-4">
-                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                            {{ __('Пациенты санатория') }}
-                        </h2>
+                    @if(empty($logs))
+                        <header>
+                            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                {{ __('Записи не найдены') }}
+                            </h2>
+                        </header>
+                    @else
+                        <header class="mb-4">
+                            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                {{ __('Пациенты санатория') }}
+                            </h2>
 
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            {{ __("В таблице представлена краткая информация о пациентах. При необходимости взаимодействия с данными, выберите необходимое действие в соответствующей колонке.") }}
-                        </p>
-                    </header>
-                    <table class="table-auto border-collapse border border-gray-300 w-full">
-                        <thead>
-                        <tr>
-                            <th class="border border-gray-300 px-4 py-2">Дата приема</th>
-                            <th class="border border-gray-300 px-4 py-2">Время приема</th>
-                            <th class="border border-gray-300 px-4 py-2">ФИО</th>
-                            <th class="border border-gray-300 px-4 py-2">Дата рождения</th>
-                            <th class="border border-gray-300 px-4 py-2">Медицинская карта</th>
-                            <th class="border border-gray-300 px-4 py-2">Действие</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($logs as $log)
+                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                {{ __("В таблице представлена краткая информация о пациентах. При необходимости взаимодействия с данными, выберите необходимое действие в соответствующей колонке.") }}
+                            </p>
+                        </header>
+                        <table class="table-auto border-collapse border border-gray-300 w-full">
+                            <thead>
                             <tr>
-                                <td class="border border-gray-300 px-4 py-2">{{ \Carbon\Carbon::parse($log->log_receipt->date_receipt)->locale('ru')->translatedFormat('D, d M Y') }}</td>
-                                <td class="border border-gray-300 px-4 py-2">{{ \Carbon\Carbon::parse($log->log_receipt->time_receipt)->locale('ru')->translatedFormat('H:i') }}</td>
-                                <td class="border border-gray-300 px-4 py-2">{{ $log->patient->name }}</td>
-                                <td class="border border-gray-300 px-4 py-2">{{ \Carbon\Carbon::parse($log->patient->birth_day)->locale('ru')->translatedFormat('d M Y') }}</td>
-                                <td class="border border-gray-300 px-4 py-2">{{ $log->patient->medical_card }}</td>
-                                <td class="border border-gray-300 px-4 py-2">
-                                    <button onclick="toggleDropdown(this)" class="text-gray-600 focus:outline-none">⋮</button>
-                                    <div class="dropdown-menu absolute right-0 hidden bg-white border border-gray-300 mt-1">
-                                        <ul class="list-none p-2">
-                                            <li class="border"><a href="{{ route('log.edit', ['id' => $log->id]) }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Редактировать</a></li>
-                                            <li class="border block px-4 py-2 text-gray-800 hover:bg-gray-200">
-                                                <form action="{{ route('log.destroy', ['id' => $log->id]) }}" method="post" onsubmit="return confirmDeletion()">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <input type="submit" value="Удалить">
-                                                </form>
-                                            </li>
-                                            <li class="border"><a href="{{ route('log.find', ['id' => $log->id]) }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Просмотр</a></li>
-                                        </ul>
-                                    </div>
-                                </td>
+                                <th class="border border-gray-300 px-4 py-2">Дата приема</th>
+                                <th class="border border-gray-300 px-4 py-2">Время приема</th>
+                                <th class="border border-gray-300 px-4 py-2">ФИО</th>
+                                <th class="border border-gray-300 px-4 py-2">Дата рождения</th>
+                                <th class="border border-gray-300 px-4 py-2">Медицинская карта</th>
+                                <th class="border border-gray-300 px-4 py-2">Действие</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @foreach ($logs as $log)
+                                <tr>
+                                    <td class="border border-gray-300 px-4 py-2">{{ \Carbon\Carbon::parse($log->log_receipt->date_receipt)->locale('ru')->translatedFormat('D, d M Y') }}</td>
+                                    <td class="border border-gray-300 px-4 py-2">{{ \Carbon\Carbon::parse($log->log_receipt->time_receipt)->locale('ru')->translatedFormat('H:i') }}</td>
+                                    <td class="border border-gray-300 px-4 py-2">{{ $log->patient->name }}</td>
+                                    <td class="border border-gray-300 px-4 py-2">{{ \Carbon\Carbon::parse($log->patient->birth_day)->locale('ru')->translatedFormat('d M Y') }}</td>
+                                    <td class="border border-gray-300 px-4 py-2">{{ $log->patient->medical_card }}</td>
+                                    <td class="border border-gray-300 px-4 py-2">
+                                        <button onclick="toggleDropdown(this)" class="text-gray-600 focus:outline-none">⋮</button>
+                                        <div class="dropdown-menu absolute right-0 hidden bg-white border border-gray-300 mt-1">
+                                            <ul class="list-none p-2">
+                                                <li class="border"><a href="{{ route('log.edit', ['id' => $log->id]) }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Редактировать</a></li>
+                                                <li class="border block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                                    <form action="{{ route('log.destroy', ['id' => $log->id]) }}" method="post" onsubmit="return confirmDeletion()">
+                                                        @csrf
+                                                        @method('delete')
+                                                        <input type="submit" value="Удалить">
+                                                    </form>
+                                                </li>
+                                                <li class="border"><a href="{{ route('log.find', ['id' => $log->id]) }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Просмотр</a></li>
+                                            </ul>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
